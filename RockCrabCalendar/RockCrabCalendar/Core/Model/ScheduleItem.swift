4544e28ccf71180d3f966ae0c266f27107e54a4c
cys <marine8743@naver.com>
2025-07-23 15:36:55 +0900

    feat: 앱 구조 수정

---
+++ RockCrabCalendar/RockCrabCalendar/Core/Model/ScheduleItem.swift
@@ -0,0 +1,10 @@
+//
+//  ScheduleItem.swift
+//  RockCrabCalendar
+//
+//  Created by YuSeongChoi on 7/23/25.
+//
+
+import Foundation
+
+
---
+++ RockCrabCalendar/RockCrabCalendar/Featrues/Schedule/QWERFetcher.swift
@@ -0,0 +1,8 @@
+//
+//  QWERFetcher.swift
+//  RockCrabCalendar
+//
+//  Created by YuSeongChoi on 7/23/25.
+//
+
+import Foundation
